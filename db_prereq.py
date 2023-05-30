1173325ab132a880877a992f1dc1ef74bbf55229
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2023-05-30 16:41:57 +1100

    Stage 3

--- db_prereq.py
+++ db_prereq.py
@@ -1,21 +1,40 @@
-id_, text, unique, not_null = 'INTEGER PRIMARY KEY AUTOINCREMENT', \
-                              'VARCHAR(20)', 'UNIQUE', \
-                              'NOT NULL'
+id_, text, integer, unique, not_null = 'INTEGER PRIMARY KEY AUTOINCREMENT', \
+                                       'VARCHAR(20)', \
+                                       'INTEGER', \
+                                       'UNIQUE', \
+                                       'NOT NULL'
 
+templates = {'create': 'CREATE TABLE {table} ({columns});',
+             'insert': 'INSERT INTO {table} ({columns}) VALUES ({values});',
+             'select_from': 'SELECT {columns} FROM {table};',
+             'max_row': 'SELECT max(rowid) FROM {table};'}
+
+# specify column name as tuple ('parent table', 'parent column') to make it a FK
 tables = {
-    'meals': {
-        'id': id_, 'name': ' '.join((text, unique, not_null))
-    },
     'ingredients': {
         'id': id_, 'name': ' '.join((text, unique, not_null))
     },
     'measures': {
         'id': id_, 'name': ' '.join((text, unique))
     },
+    'meals': {
+        'id': id_, 'name': ' '.join((text, unique, not_null))
+    },
     'recipes': {
         'id': id_, 'name': ' '.join((text, not_null)), 'description': text
+    },
+    'serve': {
+        'id': id_,
+        ('meals', 'id'): ' '.join((integer, not_null)),
+        ('recipes', 'id'): ' '.join((integer, not_null))
     }
 }
+
+relations = ({'objects': ('meals', 'recipes'),
+              'prompts': ('When the dish can be served', 'What dishes fit this meal'),
+              'link': 'serve', 'options': 'name'
+              },
+             )
 
 init_data = {
     "meals": (
